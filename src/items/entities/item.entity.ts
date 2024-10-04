f274789abd8dc52a3f5d8ae46d3f7e049235d07a
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-10-04 17:31:40 +0500

    Listing OneToOne

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Listing } from "./listing";
 
 @Entity()
 export class Item {
@@ -11,7 +12,11 @@ export class Item {
     @Column({ default: true })
     public: boolean;
 
+    @OneToOne(() => Listing)
+    @JoinColumn()
+    listing: Listing;
+
     constructor(item: Partial<Item>) {
-        Object.assign(this, item)
+        Object.assign(this, item);
     }
 }
